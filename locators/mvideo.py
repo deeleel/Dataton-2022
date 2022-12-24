5f422a2c22dbf1844401a6dc38e284637e883474
Diana Li <dlee9370@gmail.com>
2022-12-24 18:17:01 +0300

    halfway

--- locators/mvideo.py
+++ locators/mvideo.py
@@ -6,9 +6,35 @@ all_features = (By.XPATH, '//*[contains(@class, "characteristics-link")]')
 # need features!
 # ...
 
-guarantee = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Гарантия"]]//*[@class="item-with-dots__text ng-star-inserted"]')
-country = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Страна"]]//*[@class="item-with-dots__text"]')
+priceNow = (By.XPATH, '//*[@class="price__main-value"]')
+priceOriginal = (By.XPATH, '//*[contains(@class, "price__sale-value")]')
 
-series = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Серия"]]//*[@class="item-with-dots__text"][last()]')
-os = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Операционная "]]//*[@class="item-with-dots__text"]')
+guarantee = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Гарантия"]]//*[@class="item-with-dots__value"]')
+country = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Страна"]]//*[@class="item-with-dots__value"]')
+
+series = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Серия"]]//*[@class="item-with-dots__value"]')
+os = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Операционная "]]//dd')
+os_windows = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Windows"]]//dd')
+
+processor_cores = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="ядер"]]//*[@class="item-with-dots__value"]')
+processor_model = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Модель "] and descendant::*[text()="процессора"]]//*[@class="item-with-dots__value"]')
+processor_type = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Тип "] and descendant::*[text()="процессора"]]//*[@class="item-with-dots__value"]')
+processor_frequency = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Частота "] and descendant::*[text()="процессора"]]//*[@class="item-with-dots__value"]')
+
+gpu_manufacturer = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Производитель "] and descendant::*[text()="видеопроцессора"]]//*[@class="item-with-dots__value"]')
+gpu_controller = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Графический "] and descendant::*[text()="контроллер"]]//*[@class="item-with-dots__value"]')
+gpu_capacity = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Объем "] and descendant::*[text()="видеопамяти"]]//*[@class="item-with-dots__value"]')
+
+screen_size = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Диагональ "] and descendant::*[text()="экрана"]]//*[@class="item-with-dots__value"]')
+screen_frequency = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Частота "] and descendant::*[text()="обновления"]]//*[@class="item-with-dots__value"]')
+
+ssd_size = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Объем "] and descendant::*[text()="SSD"]]//*[@class="item-with-dots__value"]')
+ram = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Оперативная память "]]//*[@class="item-with-dots__value"]')
+
+ram_type = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Тип оперативной "] and descendant::*[text()="памяти"]]//*[@class="item-with-dots__value"]')
+
+hdmi = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Выход "] and descendant::*[text()="HDMI"]]//*[@class="item-with-dots__text"]')
+material = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Материал "] and descendant::*[text()="корпуса"]]//*[@class="item-with-dots__value"]')
+battery = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Работа от "] and descendant::*[text()="аккумулятора"]]//*[@class="item-with-dots__value"]')
+weight = (By.XPATH, '//mvideo-item-with-dots[descendant::*[text()="Вес"]]//*[@class="item-with-dots__value"]')
 
--- marketplaces/mvideo_base.py
+++ marketplaces/mvideo_base.py
@@ -12,6 +12,15 @@ class Mvideo(BaseFunctions):
 
     def get_features_mv(self):
         data = {}
+
+        # Цена со скидкой
+        priceNow = self.find(mv_locators.priceNow) # надо обработать TimeoutException
+        data['PriceNow'] = priceNow.text
+
+        # Цена со скидкой
+        priceOriginal = self.find(mv_locators.priceOriginal)
+        data['PriceOriginal'] = priceOriginal.text
+
         # Гарантия
         guarantee = self.find(mv_locators.guarantee)
         data['Guarantee'] = guarantee.text
@@ -21,13 +30,79 @@ class Mvideo(BaseFunctions):
         data['Country'] = country.text
 
         # Серия
-        series = self.find(mv_locators.series)
+        series = self.find(mv_locators.series) # need update!!
         data['Series'] = series.text
 
         # ОС: установлена -> не установлена
         os = self.find(mv_locators.os)
         data['OS'] = os.text
 
-        
+        # OC windows
+        windows = self.find(mv_locators.os_windows)
+        data['windows'] = windows.text
+
+        # Кол-во ядер
+        cores = self.find(mv_locators.processor_cores)
+        data['CoresNumber'] = cores.text
+
+        # Модель процессора
+        processor_model = self.find(mv_locators.processor_model)
+        data['ProcessorModel'] = processor_model.text
+
+        # Тип процессора
+        processor_type = self.find(mv_locators.processor_type)
+        data['ProcessorType'] = processor_type.text
+
+        # Частота процессора
+        processor_frequency = self.find(mv_locators.processor_frequency)
+        data['ProcessorFrequency'] = processor_frequency.text
+
+        # GpuManufacturer
+        gpu_manufacturer = self.find(mv_locators.gpu_manufacturer)
+        data['GpuManufacturer'] = gpu_manufacturer.text
+
+        # gpu_controller
+        gpu_controller = self.find(mv_locators.gpu_controller)
+        data['GpuController'] = gpu_controller.text
+
+        # gpu_capacity
+        gpu_capacity = self.find(mv_locators.gpu_capacity)
+        data['GpuCapacity'] = gpu_capacity.text
+
+        # screen_size
+        screen_size = self.find(mv_locators.screen_size)
+        data['ScreenSize'] = screen_size.text
+
+        # screen_frequency
+        screen_frequency = self.find(mv_locators.screen_frequency)
+        data['ScreenFrequency'] = screen_frequency.text
+
+        # ssd_size
+        ssd_size = self.find(mv_locators.ssd_size)
+        data['SsdSize'] = ssd_size.text
+
+        # ram
+        ram = self.find(mv_locators.ram)
+        data['RAM'] = ram.text
+
+        # ram_type
+        ram_type = self.find(mv_locators.ram_type)
+        data['RAM_type'] = ram_type.text
+
+        # hdmi
+        hdmi = self.find(mv_locators.hdmi)
+        data['HDMI'] = hdmi.text
+
+        # material
+        material = self.find(mv_locators.material)
+        data['Material'] = material.text
+
+        # battery
+        battery = self.find(mv_locators.battery)
+        data['Battery'] = battery.text
+
+        # weight
+        weight = self.find(mv_locators.weight)
+        data['Weight'] = weight.text
 
         print(data)
